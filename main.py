1afdb71c979fbe8f524a69d9ff77810af1ea4244
Joni135 <jonathan.benjamin.zeller@googlemail.com>
2020-10-01 15:36:10 +0200

    Added Ascii-Art at startup

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("")
-print("Welcome to get a job. Here you try to get your job of choice by learning new skills in different fields of study.")
+import time
+
+
+# Displays the title message as ascii-art
+# @param speed: how fast to build up the startup-message, in seconds
+def title_ascii_art(speed):
+    print("//===   ||====  =======         //\\\\             ||  //===\\\\  ||==\\\\")
+    time.sleep(speed)
+    print("||      ||         ||          //  \\\\            ||  ||   ||  ||  ||")
+    time.sleep(speed)
+    print("|| ===  ||===      ||         //====\\\\           ||  ||   ||  ||==<")
+    time.sleep(speed)
+    print("||  ||  ||         ||        //      \\\\      ||  //  ||   ||  ||  ||")
+    time.sleep(speed)
+    print("\\\\==//  ||====     ||       //        \\\\     \\\\===   \\\\===//  ||==//")
+    time.sleep(speed)
+    print("")
+    time.sleep(speed)
+    print("Welcome to get a job. Here you try to get your job of choice by learning new skills in different fields of study.")
+
+title_ascii_art(0.2)
