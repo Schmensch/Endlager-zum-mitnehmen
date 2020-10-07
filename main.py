743cd00ae50fbc82c71da4d80c1f5304869be268
Jonathan Zeller <jonathan.benjamin.zeller@googlemail.com>
2020-10-07 19:01:43 +0200

    Fixed big with "ende" not exiting the game

--- main.py
+++ main.py
@@ -11,16 +11,27 @@ story = json.loads(data.read())
 
 decision = "start"
 
+ende = 0
+
 while True:
-    while True:
-        try:
+    if ende == 1:
+        break
+    else:
+        while True:
+            try:
 
-            decision_data = story[decision]
-            print(decision_data["text"])
-            answer = input()
-            print("\n")
-            decision = decision_data[answer]
+                decision_data = story[decision]
+                print(decision_data["text"])
 
-            break
-        except:
-            print("Bitte eine valide Antwort eingeben. \n")
+                if decision == "ende":
+                    ende = 1
+                    break
+
+                answer = input()
+                print("\n")
+                decision = decision_data[answer]
+
+                break
+
+            except:
+                print("Bitte eine valide Antwort eingeben. \n")
